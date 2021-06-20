252cc04155aad67299903a6216680114d9b4e941
Michael Olatunji <tundexmike@gmail.com>
2021-06-20 23:17:17 +0100

    feat: dynamic routes, style page

--- story.go
+++ story.go
@@ -3,14 +3,16 @@ package cyoa
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"strings"
 )
 
 var tpl *template.Template
 
-func init(){
+func init() {
 	tpl = template.Must(template.New("").Parse(defaultHandlerTemplate))
 }
 
@@ -21,6 +23,45 @@ const defaultHandlerTemplate = `
 		<meta charset="utf-8" />
 		<title> Choose Your Own Adventure </title>
 	</head>
+	<style>
+		body {
+			font-family: helvetica, arial, tahoma, sans-serif;
+		}
+		h1 {
+			text-align: center;
+			position: relative;
+		}
+		.page {
+			width: 80%;
+			max-width: 500px;
+			margin: auto;
+			margin-top: 40px;
+			margin-bottom: 40px;
+			padding: 80px;
+			background: #fffcf6;
+			border: 1px solid #eee;
+			box-shadow: 0 10px 6px -6px #777;
+		}
+		ul {
+			border-top: 1px dotted #ccc;
+			padding: 10px 0 0 0;
+			-webkit-padding-start: 0;
+		}
+		li {
+			padding-top: 10px;
+		}
+		a, a:visited {
+			text-decoration: none;
+			color: #6295b5;
+		}
+		a:active, a:hover {
+			color: #7792a2;
+		}
+		p {
+			text-indent: 1em;
+			color: #616161;
+		}
+	</style>
 	<body>
 	<h1>{{.Title}}</h1>
 	{{range .Story}}
@@ -66,10 +107,22 @@ func NewHandler(s Story) http.Handler {
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := tpl.Execute(w, h.s["intro"])
-	if err != nil {
-		panic(err)
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
 	}
+	path = path[1:]
+
+	if chapter, ok := h.s[path]; ok {
+		err := tpl.Execute(w, chapter)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(w, "Chapter not found.", http.StatusNotFound)
+
 }
 
 func defaultMux() *http.ServeMux {
